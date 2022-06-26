22683716f69a7af7ea8b4f905a4f4f053c21204a
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-06-26 20:55:00 +0630

    2.1: Course information step6
    Create Course component and move Header and Copntent components to it.
    Modify Content component to show any number of parts that  course has.
    Comment the code  where Total component is called.

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,76 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  const totalExercises = course.parts.reduce((pval, cval) =>  pval+cval.exercises ,0);
+
+  return (
+    <Course course={course}/>
+  );
+}
+
+const Course = (props) =>{
+  return(
+    <>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+      {/* <Total exercises={totalExercises} /> */}
+    </>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  );
+}
+
+const Content = (props) => {
+  return (
+    <>
+      {props.parts.map(part => (
+        <Part 
+          key={part.id} 
+          part={part.name} e
+          xercises={part.exercises} />
+      ))}
+    </>
+  );
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.exercises}
+    </p>
+  );
+}
+
+export default App;
