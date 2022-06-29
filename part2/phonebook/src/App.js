91322ed012327be570331843ceb5816254cd81d8
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-06-29 21:39:42 +0630

    Add filter input element and add state and onChange for it

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,12 +6,15 @@ const Person = (props) => (
   </>
 )
 const App = () => {
-  const [persons, setPersons] = useState([{ 
-      name: 'Arto Hellas' ,
-      number: '040-1234567',  
-  }]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber,setNewNumber]=useState('')
+  const [filter, setFilter]=useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -19,6 +22,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
       setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) =>{
+    setFilter(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -29,16 +36,25 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       return;
     }
+
     setPersons(persons.concat({
       name:newName,
       number:newNumber,
+      id:persons[persons.length-1].id +1
     }))
     setNewName('')
   }
 
+  // const personsToShow = 
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with<input
+                          onChange={handleFilterChange}
+                          value={filter}
+                        />
+      <h3>add a new</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
