74639660120819f75c4f6283c9285babc8efde1b
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-07-04 20:56:01 +0630

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Filter = (props) => (
@@ -38,23 +38,19 @@ const Persons = (props) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber,setNewNumber]=useState('')
   const [filter, setFilter]=useState('')
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/notes')
+      .get('http://localhost:3001/persons')
       .then(response => {
+        console.log('%cApp.js line:50 response.data', 'color: #007acc;', response.data);
         setPersons(response.data)
       })
-  })
+  },[])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
