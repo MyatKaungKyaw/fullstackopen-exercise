9c77e4ac97634912b78d63e4580b87a7665df3ea
Lu Mike <33856024+MyatKaungKyaw@users.noreply.github.com>
2022-07-04 14:02:31 +0630

    !Not tested yet. Add data from db.json to persons after first render.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => (
   <>
@@ -46,6 +47,14 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber,setNewNumber]=useState('')
   const [filter, setFilter]=useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setPersons(response.data)
+      })
+  })
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
