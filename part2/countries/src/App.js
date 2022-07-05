ab71a38249045ed2fb8ae29f2c9f539a23931b10
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-07-05 21:26:43 +0630

    30% finish to show find countries result

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,24 +1,54 @@
-import {useState, useEffect} from 'react'
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 const FindCountries = (props) => (
-  <div>
+  <div className={'find-countries'}>
     <p>find countries</p>
-    <input/>
+    <input
+      value={props.value}
+      onChange={props.onChange}
+    />
   </div>
 )
 
-const ShowCountries = () => (
-  <div>
-
-  </div>
-)
+const ShowCountries = (props) => {
+  if (props.countries.length = 0 || props.countries.length > 10) {
+    return (
+      < div >
+        <p>Too many matches, specify another filter</p>
+      </div >
+    )
+  }
+  return(
+    <div></div>
+  )
+}
 
 const App = () => {
   const [find, setFind] = useState('')
-  return(
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => setCountries(response.data))
+  },[])
+
+  const handleFindChange = (event) => {
+    setFind(event.target.value)
+  }
+
+  const foundCountries = countries
+
+  return (
     <div>
-      
+      <FindCountries
+        value={find}
+        onChange={handleFindChange}
+      />
+      <ShowCountries
+        countries={foundCountries}
+      />
     </div>
   )
 }
