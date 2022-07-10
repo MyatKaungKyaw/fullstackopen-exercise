d22b4ff1136571bfe94bf1c752decc2e8e22a57a
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-07-10 22:39:21 +0630

    get data to show weather condition

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,71 +1,50 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css';
-const FindCountries = (props) => (
-  <div className={'find-countries'}>
-    <p>find countries</p>
-    <input
-      value={props.value}
-      onChange={props.onChange}
-    />
-  </div>
-)
+import { FindCountries, ShowCountries} from './Components/Country'
 
-const ShowCountries = (props) => {
-  if (props.countries.length === 0 || props.countries.length > 10) {
-    return (
-      <div >
-        <p>Too many matches, specify another filter</p>
-      </div >
-    )
-  }
-  else if(props.countries.length > 1){
-    return(
-      <ul className={'multi-countries'}>
-        {props.countries.map(country => 
-          <li key={country.fifa}>
-            <p className={'country-multi'}>{country.name.common}</p>
-            <button  onClick={e => props.handleShowCtryOnClick(e, country.fifa)}>show</button>
-            {props.showCtryDetail[country.fifa] === true ? <CountryDetail country={country}/> : null}
-          </li>
-        )}
-      </ul>
-    )
-  }
-
-  const country = props.countries[0];
-  return(
-    <div>
-      <h2>{country.name.common}</h2>
-      <CountryDetail country={country}/>
-    </div>
-  )
-}
-
-const CountryDetail = ({country}) => (
-  <>
-    <p>
-      capital {country.capital.reduce((pval, cval, index, arr)=> arr.length === index ? pval : `${pval}, ${cval}`)}<br/>
-      area {country.area}
-    </p>
-    <h3>languages:</h3>
-    <ul>
-      {Object.getOwnPropertyNames(country.languages).map(lan => <li key={lan}>{country.languages[lan]}</li>)}
-    </ul>
-    <object type="image/svg+xml" data={country.flags.svg} className={'flag'}>flag</object>
-  </>
-)
+const weather_api_key = process.env.REACT_APP_WEATHER_API_KEY
 
 const App = () => {
   const [find, setFind] = useState('')
-  const [countries, setCountries] = useState([])
+  const [allCtry, setAllCtry] = useState([])
+  const [foundCtry, setFoundCtry] = useState([])
   const [showCtryDetail, setShowCtryDetail] = useState({})
+  // const [weatherCtys, setWeatherCtys] = useState([])
+  const [weather, setWeather]=useState([])
 
   useEffect(() => {
+    console.log('[]');
     axios
       .get('https://restcountries.com/v3.1/all')
-      .then(response => setCountries(response.data))
+      .then(response => setAllCtry(response.data))
   },[])
+
+  useEffect(() => {
+    console.log('find');
+    const ctrys = allCtry.filter(country => country.name.common.toLowerCase().includes(find.toLowerCase()))
+    setFoundCtry(ctrys)
+  },[find,allCtry])
+
+  useEffect(() => {
+    console.log('foundCtry');
+    if(foundCtry.length !== 1) return
+    
+    const capitalLatLng = foundCtry[0].capitalInfo.latlng
+    console.log('%cApp.js line:94 capitalLatLng', 'color: #007acc;', capitalLatLng);
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${capitalLatLng['0']}&lon=${capitalLatLng['1']}&appid=${weather_api_key}&units=metric`)
+      .then(response => {
+        const data  = response.data
+        setWeather({
+          city: foundCtry[0].capital['0'],
+          temp: data.main.temp,
+          wind: data.wind.speed,
+          icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`,
+        })
+      })
+      .catch(err => console.error(err))
+  },[foundCtry])
 
   const handleFindChange = (event) => {
     setFind(event.target.value.toString().trim())
@@ -80,13 +59,11 @@ const App = () => {
     setShowCtryDetail(objShowCtry)
   }
 
-  const foundCountries = find === '' 
-  ? countries
-  : countries.filter(country => country.name.common.toLowerCase().includes(find.toLowerCase()))
+  console.log('%cApp.js line:105 weather', 'color: #007acc;', weather);
 
   // console.log(countries.reduce((pv,cv) => cv.independent === false ? pv.concat(cv.name.common) : pv,[]));
-  console.log('%cApp.js line:64 foundCountries', 'color: #007acc;', foundCountries);
-
+  console.log('%cApp.js line:64 foundCtry', 'color: #007acc;', foundCtry);
+  
   return (
     <div>
       <FindCountries
@@ -94,9 +71,10 @@ const App = () => {
         onChange={handleFindChange}
       />
       <ShowCountries
-        countries={foundCountries}
+        countries={foundCtry}
         showCtryDetail={showCtryDetail}
         handleShowCtryOnClick={handleShowCtryOnClick}
+        weather={weather}
       />
     </div>
   )
